782f7495d233f8d25b36476cacd1a81e70113559
Gazi-Saiful-Islam <gazisaifulislam894@gmail.com>
2023-12-15 02:49:31 +0300

    js loop practice

---
+++ index.js
@@ -0,0 +1,71 @@
+
+// practicing loop
+
+/*
+
+var sum = 0;
+for (var i = 1; i<=10; i++){
+  sum= sum+i
+console.log(sum + ' + ' + i + ' = ' + (sum+i))
+
+}
+
+*/
+
+/*
+
+var n =10;
+for(var i = 1; i<=n; i++){
+  var result =' '
+  for(var x =1; x<=n; x++){
+   result+= x+' '
+    console.log (result)
+  }
+}
+
+*/
+
+// while loop
+
+var rand = Math.floor(Math.random()*10 +1)
+while(rand==4){
+
+console.log('it has matched')
+rand++;
+}
+
+console.log(rand)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
